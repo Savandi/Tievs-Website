c552120ec8bbdd254f66ecff20124d9ec7c59527
K S S Bandaranayake <it18113532@my.sliit.lk>
2021-10-27 02:41:42 +0530

    System architectures front definition writing and technologies preview placement adjust

--- agency/js/script.js
+++ agency/js/script.js
@@ -90,6 +90,20 @@ jQuery(function ($) {
         });
     }
 
+    if ($(".js-rotating").length) {
+        $(".js-rotating").Morphext({
+            // The [in] animation type. Refer to Animate.css for a list of available animations.
+            animation: "flipInX",
+            // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+            separator: ",",
+            // The delay between the changing of each phrase in milliseconds.
+            speed: 5500,
+            complete: function () {
+                // Called after the entrance animation is executed.
+            }
+        });
+    }
+
 
     /* ===================================
       Side Menu
